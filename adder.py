2f48e1fdeff18ac694487057d491be7c793fe193
Onyx8 <89249857+Onyx8@users.noreply.github.com>
2021-12-08 19:42:36 +0800

    Update adder.py

--- adder.py
+++ adder.py
@@ -24,10 +24,10 @@ print (cy+"Usaha Tidak Pernah Menghianati Hasil ^^")
 print (gr+"By : Onyx Sitorus")
 
 print (re+"NOTE :")
-print ("1. Telegram only allow to add 200 members in group by one user.")
+print ("1. Telegram only allow to add 50 members in group by one user.")
 print ("2. You can Use multiple Telegram accounts for add more members.")
 print ("3. Add only 50 members in group each time otherwise you will get flood error.")
-print ("4. Then wait for 15-30 miniute then add members again.")
+print ("4. Then wait for next day then add members again.")
 print ("5. Make sure you enable Add User Permission in your group")
 
 cpass = configparser.RawConfigParser()
